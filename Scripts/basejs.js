d5f306752e7e7caed8bb8283f2cf87c9ff92775b
SquareZeb <136224084+SquareZeb@users.noreply.github.com>
2023-10-02 02:16:46 -0600

    Add files via upload

---
+++ Scripts/basejs.js
@@ -0,0 +1,44 @@
+﻿const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+    tab.addEventListener('mouseenter', () => {
+        tab.classList.add('active');
+    });
+
+    tab.addEventListener('mouseleave', () => {
+        tab.classList.remove('active');
+    });
+
+    tab.addEventListener('mouseover', () => {
+        const generatedLinks = tab.querySelector('.generated-links');
+        generatedLinks.style.top = `${tab.offsetTop}px`;
+    });
+});
+
+function openImage(img) {
+    var zoomedImg = document.getElementById("zoomed-img");
+    zoomedImg.src = img.src;
+    document.querySelector(".image-zoom").classList.add("active");
+}
+
+function closeImage() {
+    document.querySelector(".image-zoom").classList.remove("active");
+}
+
+const audioPlayer = document.getElementById('audioPlayer');
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+const stopButton = document.getElementById('stopButton');
+
+playButton.addEventListener('click', () => {
+    audioPlayer.play();
+});
+
+pauseButton.addEventListener('click', () => {
+    audioPlayer.pause();
+});
+
+stopButton.addEventListener('click', () => {
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+});
---
+++ Scripts/warning.js
@@ -0,0 +1,18 @@
+﻿document.write(`
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <div class="background-box"></div>
+    <header></header>
+    <h3 style="text-align:center; font-style:oblique;">Credits:</h3>
+    <p style="text-align:center;color:red;">Note: This game has not been fully released and there may be secrets and easter eggs that have not been discovered which may render this archive outdated.</p>
+    <p style="text-align:center;color:lawngreen;"><a href="https://mrdrnose.itch.io/votv">Voices of the Void:</a></p>
+    <p style="text-align:center;color:lawngreen;">If any new secret or hidden easter egg pops up you may contact me through <a href="https://discordapp.com/users/813250158384906260">here:</a></p>
+    <div class="image-zoom" onclick="closeImage()">
+        <img id="zoomed-img" src="" alt="Zoomed Image">
+    </div>
+</body>
+</html>
+`);
